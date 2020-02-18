e21fbcb2f7b4f9a66a2d262c173489fb33a35d98
Fischerboot <Fischerboot-sinkt@web.de>
2020-02-18 21:46:49 +0100

    Shift register prepared

--- BasicTemplate/BasicTemplate.ino
+++ BasicTemplate/BasicTemplate.ino
@@ -20,6 +20,13 @@ can not be read by observers.
 // Onboard LED I/O pin on NodeMCU board
 const int PIN_LED = 2; // D4 on NodeMCU and WeMos. Controls the onboard LED.
 
+//Pin connected to ST_CP of 74HC595
+int latchPin = 8;
+//Pin connected to SH_CP of 74HC595
+int clockPin = 12;
+////Pin connected to DS of 74HC595
+int dataPin = 11;
+
 void setup() {
   // put your setup code here, to run once:
   // initialize the LED digital pin as an output.
@@ -45,7 +52,7 @@ void setup() {
      //if you get here you have connected to the WiFi
      Serial.println("connected...yeey :)");
      }
- digitalWrite(PIN_LED, HIGH); // Turn led off as we are not in configuration mode.
+  digitalWrite(PIN_LED, HIGH); // Turn led off as we are not in configuration mode.
       // For some unknown reason webserver can only be started once per boot up 
       // so webserver can not be used again in the sketch.
     
@@ -108,6 +115,8 @@ void setup() {
     }
   });
   ArduinoOTA.begin();
+
+   pinMode(latchPin, OUTPUT);
 }
 
 
@@ -119,4 +128,50 @@ void loop() {
   digitalWrite(PIN_LED, LOW);
  
   
+}
+
+void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
+  // This shifts 8 bits out MSB first, 
+  //on the rising edge of the clock,
+  //clock idles low
+
+  //internal function setup
+  int i=0;
+  int pinState;
+  pinMode(myClockPin, OUTPUT);
+  pinMode(myDataPin, OUTPUT);
+
+  //clear everything out just in case to
+  //prepare shift register for bit shifting
+  digitalWrite(myDataPin, 0);
+  digitalWrite(myClockPin, 0);
+
+  //for each bit in the byte myDataOut�
+  //NOTICE THAT WE ARE COUNTING DOWN in our for loop
+  //This means that %00000001 or "1" will go through such
+  //that it will be pin Q0 that lights. 
+  for (i=7; i>=0; i--)  {
+    digitalWrite(myClockPin, 0);
+
+    //if the value passed to myDataOut and a bitmask result 
+    // true then... so if we are at i=6 and our value is
+    // %11010100 it would the code compares it to %01000000 
+    // and proceeds to set pinState to 1.
+    if ( myDataOut & (1<<i) ) {
+      pinState= 1;
+    }
+    else {  
+      pinState= 0;
+    }
+
+    //Sets the pin to HIGH or LOW depending on pinState
+    digitalWrite(myDataPin, pinState);
+    //register shifts bits on upstroke of clock pin  
+    digitalWrite(myClockPin, 1);
+    //zero the data pin after shift to prevent bleed through
+    digitalWrite(myDataPin, 0);
+  }
+
+  //stop shifting
+  digitalWrite(myClockPin, 0);
 }
