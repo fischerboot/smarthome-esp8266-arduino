17cc2fff92595a834baf151cc0d8545de5989004
Fischerboot <Fischerboot-sinkt@web.de>
2020-02-21 05:23:55 +0100

    broken try

--- BasicTemplate/BasicTemplate.ino
+++ BasicTemplate/BasicTemplate.ino
@@ -22,13 +22,6 @@ can not be read by observers.
 // Onboard LED I/O pin on NodeMCU board
 const int PIN_LED = 2; // D4 on NodeMCU and WeMos. Controls the onboard LED.
 
-//Pin connected to RCLK of 74HC595
-int latchPin = 12;
-//Pin connected to SRCLK of 74HC595
-int clockPin = 13;
-////Pin connected to SER of 74HC595
-int dataPin = 15;
-
 SevSeg sevseg; //Instantiate a seven segment controller object
 
 void setup() {
@@ -120,11 +113,11 @@ void setup() {
   });
   ArduinoOTA.begin();
 
-  pinMode(latchPin, OUTPUT);
+  pinMode(12, OUTPUT); //latch
   //SevenSeg Settings 
   byte numDigits = 4;
-  byte digitPins[] = {2, 3, 4, 5}; // TBD 
-  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13}; // TBD
+  byte digitPins[] = {16, 4, 5, 14}; // TBD D0, D2, D1,D5
+  byte segmentPins[] = {1, 2, 3, 4, 5, 6, 7, 8}; // TBD
   bool resistorsOnSegments = false; // 'false' means resistors are on digit pins
   byte hardwareConfig = COMMON_ANODE; // See README.md for options
   bool updateWithDelays = false; // Default 'false' is Recommended
@@ -138,58 +131,21 @@ void setup() {
 
 void loop() {
   ArduinoOTA.handle();
-  delay(1000);
-  digitalWrite(PIN_LED, HIGH);
-  delay(1000);
-  digitalWrite(PIN_LED, LOW);
- 
+
+  static unsigned long timer = millis();
+  static int deciSeconds = 0;
   
-}
-
-void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
-  // This shifts 8 bits out MSB first, 
-  //on the rising edge of the clock,
-  //clock idles low
-
-  //internal function setup
-  int i=0;
-  int pinState;
-  pinMode(myClockPin, OUTPUT);
-  pinMode(myDataPin, OUTPUT);
-
-  //clear everything out just in case to
-  //prepare shift register for bit shifting
-  digitalWrite(myDataPin, 0);
-  digitalWrite(myClockPin, 0);
-  digitalWrite(latchPin, 0); // TBV
-
-  //for each bit in the byte myDataOut�
-  //NOTICE THAT WE ARE COUNTING DOWN in our for loop
-  //This means that %00000001 or "1" will go through such
-  //that it will be pin Q0 that lights. 
-  for (i=7; i>=0; i--)  {
-    digitalWrite(myClockPin, 0);
-
-    //if the value passed to myDataOut and a bitmask result 
-    // true then... so if we are at i=6 and our value is
-    // %11010100 it would the code compares it to %01000000 
-    // and proceeds to set pinState to 1.
-    if ( myDataOut & (1<<i) ) {
-      pinState= 1;
+  if (millis() - timer >= 100) {
+    timer += 100;
+    deciSeconds++; // 100 milliSeconds is equal to 1 deciSecond
+    
+    if (deciSeconds == 10000) { // Reset to 0 after counting for 1000 seconds.
+      deciSeconds=0;
     }
-    else {  
-      pinState= 0;
-    }
-
-    //Sets the pin to HIGH or LOW depending on pinState
-    digitalWrite(myDataPin, pinState);
-    //register shifts bits on upstroke of clock pin  
-    digitalWrite(myClockPin, 1);
-    //zero the data pin after shift to prevent bleed through
-    digitalWrite(myDataPin, 0);
+    sevseg.setNumber(deciSeconds, 1);
   }
 
-  //stop shifting
-  digitalWrite(myClockPin, 0);
-  digitalWrite(latchPin, 1);
+  sevseg.refreshDisplay(); // Must run repeatedly
+ 
+  
 }
