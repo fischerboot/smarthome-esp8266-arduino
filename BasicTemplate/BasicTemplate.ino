1f928a251266deca3a6e1b9864bc1a3dc11d0b72
Fischerboot <Fischerboot-sinkt@web.de>
2020-02-23 11:38:31 +0100

    running without function

--- BasicTemplate/BasicTemplate.ino
+++ BasicTemplate/BasicTemplate.ino
@@ -136,8 +136,8 @@ void loop() {
   static unsigned long timer = millis();
   static int deciSeconds = 0;
   
-  if (millis() - timer >= 100) {
-    timer += 100;
+  if (millis() - timer >= 1000) {
+    timer += 1000;
     deciSeconds++; // 100 milliSeconds is equal to 1 deciSecond
     
     if (deciSeconds == 10000) { // Reset to 0 after counting for 1000 seconds.
