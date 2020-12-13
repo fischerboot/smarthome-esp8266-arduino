5ec1582bbf2c87a1481663abccfa8e3262381d4b
Marcel Fischer <Fischerboot-sinkt@web.de>
2020-12-13 13:47:44 +0100

    RFC on ESP-01 with logging improved + generic rfc channel

--- BasicTemplate/BasicTemplate.ino
+++ BasicTemplate/BasicTemplate.ino
@@ -1,7 +1,7 @@
 /*
 Configuration
 */
-const char* versionStr = "20201125v0.10";
+const char* versionStr = "20201213v0.11";
 #define LoggingWithTimeout
 
 #ifdef LoggingWithTimeout
@@ -27,7 +27,7 @@ enum LogLevel{
   Warning   = 3,
   Info      = 4
 };
-static LogLevel UsedLevel = Error; 
+static LogLevel UsedLevel = Verbose; 
 //needed for library
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
@@ -40,6 +40,16 @@ static LogLevel UsedLevel = Error;
 
 // MQTT Client
 #include <PubSubClient.h>
+const char* charMqttTopic_433_Cmnd = "house/433/ardu1/cmnd";
+const char* charMqttTopic_433_State = "house/433/ardu1/state";
+
+static char jsonData[80];
+#define MAX_TOKEN_STRING 10
+static char cTokenCode [MAX_TOKEN_STRING];
+static char cTokenDevice [MAX_TOKEN_STRING];
+static char cTokenState [MAX_TOKEN_STRING];
+static char cTokenCount [MAX_TOKEN_STRING];
+static char cTokenDelay [MAX_TOKEN_STRING];
 
 // Onboard LED I/O pin on NodeMCU board
 const int PIN_LED = 2;  // D4 on NodeMCU Controls the onboard LED.
@@ -76,6 +86,158 @@ WiFiServer TelnetServer(23);
 WiFiClient TelnetClient[MAX_TELNET_CLIENTS];
 
 int bewegungsstatus=0;
+// from https://hoeser-medien.de/2017/07/openhab-433-mhz-sender-mit-json-format-ueber-mqtt-anbinden/#codesyntax_4
+// ---------------------------------------------------------------------------------------- json_lookup_char
+bool json_lookup_char ( const char* cSource, const char* cToken, char cReply[]){
+  char *ptr;
+  cReply[0]='\0'; // set null to 1st char in target
+  //Serial.print("\njson_lookup_char: ");
+  //Serial.println(cSource);
+  //Serial.print("Token >");
+  //Serial.print(cToken[0]);
+  ptr = strstr (cSource, cToken); // set pointer to 1st char in cSource matching token
+  if(!ptr){
+   logger(" json_lookup_char > ",Error);
+   logger(cToken[0],Error);
+   logger("< not found\n",Error);
+  return false;
+  }
+  logger("< ok",Verbose);
+  logger("1 *ptr >",Verbose);
+  logger(*ptr,Verbose);
+  logger("<\n",Verbose);
+  logger("Token >",Verbose);
+  logger(":\n",Verbose);
+  ptr = strstr (ptr, ":"); // set pointer to :
+  if(!ptr){
+   logger(" json_lookup_char > ",Error);
+   logger(": not found\n",Error);
+  return false;
+  }
+  logger("< ok",Verbose);
+  ptr++;
+  //if (*ptr=='"')
+  // ptr ++;
+  // read char and copy to
+  for (int i=0, x=0;i<MAX_TOKEN_STRING+1;i++){
+   if (!ptr)
+   break;
+   if (*ptr==','|| *ptr=='}') // read until ,}
+    break;
+    if (*ptr!=' '&& *ptr!='"') { // skip space + "
+      cReply[x ] = *ptr; // copy single char to target
+      cReply[x+1] = '\0'; // set end of string
+      x++;
+    }
+    ptr++;
+  } // for
+  logger(" result >",Verbose);
+  logger(cReply,Verbose);
+  logger("<\n",Verbose);
+  return true;
+}
+// from https://hoeser-medien.de/2017/07/openhab-433-mhz-sender-mit-json-format-ueber-mqtt-anbinden/#codesyntax_4
+void callback2(char* topic, byte* payload, unsigned int length) {
+  int iTokenState =0;
+  int iTokenCount =0;
+  int iTokenDelay =0;
+  #ifdef RESETWATCHDOG
+  wdt_reset();
+  #endif
+  logger("Message arrived > [",Verbose);
+  logger(topic,Verbose);
+  logger("], length: ",Verbose);
+  logger(length,Verbose);
+  logger("]",Verbose);
+  for (unsigned int i=0;i<length;i++) {
+    jsonData[i] = (char)payload[i];
+    jsonData[i+1] = '\0';
+    logger(payload[i],Verbose);
+  }
+  logger("]\n",Verbose);
+  if(strcmp(topic,charMqttTopic_433_Cmnd)==0) { // whatever you want for this topic
+    logger("MQTT > 433 cmd received ...\n",Debug);
+    // 10011 10000 für Steckdose A
+    // JSON {code:"10011", device:"10000", state:1, count:1, delay:200}
+    // MQTT Message arrived > [house/433/ardu1/cmnd], length: 71] {code:00001 , device:10000, state:1, count:3. delay:100 }
+    if( json_lookup_char(jsonData,"code" , cTokenCode) ==false) return;
+    if( json_lookup_char(jsonData,"device", cTokenDevice) ==false) return;
+    if( json_lookup_char(jsonData,"state" , cTokenState) ==false) return;
+    iTokenState = atoi(cTokenState);
+    // Serial.print ("iTokenState > ");Serial.print (iTokenState);Serial.println(" <");
+    json_lookup_char(jsonData,"count" , cTokenCount);
+    iTokenCount = atoi(cTokenCount);
+    // Serial.print ("iTokenCount > ");Serial.print (iTokenCount);Serial.println(" <");
+    if(iTokenCount==0)
+    iTokenCount = 1;
+    json_lookup_char(jsonData,"delay" , cTokenDelay);
+    iTokenDelay = atoi(cTokenDelay);
+    // Serial.print ("iTokenDelay > ");Serial.print (iTokenDelay);Serial.println(" <");
+    if(iTokenDelay==0)
+    iTokenDelay = 50;
+    logger("433-send> code>",Debug); logger(cTokenCode,Debug);
+    logger("< device>",Debug); logger(cTokenDevice,Debug);
+    logger("< state>",Debug); logger(iTokenState,Debug);
+    logger("< count>",Debug); logger(iTokenCount,Debug);
+    logger("< delay>",Debug); logger(iTokenDelay,Debug);
+    logger(" > ",Debug);
+    // 433-send> code: device: state:1 count:3 delay:100 >
+    #ifdef RESETWATCHDOG
+    wdt_reset();
+    #endif
+    switch (iTokenState)
+    {
+    case 0:
+      logger(" mySwitch.switchOff >",Debug);
+      for (int i=0;i<iTokenCount;i++) {
+        mySwitch.switchOff(cTokenCode, cTokenDevice);
+        logger("+",Debug);
+        delay(iTokenDelay);
+      }
+     client.publish(charMqttTopic_433_State,"0");
+      break;
+    case 1:
+      logger(" mySwitch.switchOn >",Debug);
+      for (int i=0;i<iTokenCount;i++) {
+        mySwitch.switchOn(cTokenCode, cTokenDevice);
+        //mySwitch.switchOn("00001", "10000");
+        logger("+",Debug);
+        delay(iTokenDelay);
+      }
+      client.publish(charMqttTopic_433_State,"1");
+      break;
+    default:
+      logger(" Error >",Error);
+      client.publish(charMqttTopic_433_State,"invalid state");
+      break;
+    }
+    logger(" DONE\n",Info);
+  } else {
+    // Switch on the LED if an 1 was received as first character
+    if ((char)payload[0] == '1') {
+      #ifndef MyESP01
+      digitalWrite(PIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+      // but actually the LED is on; this is because
+      // it is active low on the ESP-01)
+      mySwitch.switchOn("10100", "10000");
+      #else 
+      mySwitch.switchOff("10100", "10000");
+      logger("RFC Off\n",Debug);
+      //ESP.restart();
+      #endif
+    } else {
+      #ifndef MyESP01
+      digitalWrite(PIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+      mySwitch.switchOn("10100", "10000");
+      #else
+      mySwitch.switchOn("10100", "10000");
+      logger("RFC On\n",Debug);
+      //ESP.restart();
+      #endif
+    }
+  }// 433 cmd received
+  
+} // end of function
 
 void callback(char* topic, byte* payload, unsigned int length) {
   logger("Message arrived [",Verbose);
@@ -95,7 +257,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
     mySwitch.switchOn("10100", "10000");
     #else 
     mySwitch.switchOff("10100", "10000");
-    logger("RFC Off",Debug);
+    logger("RFC Off\n",Debug);
     //ESP.restart();
     #endif
   } else {
@@ -104,7 +266,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
     mySwitch.switchOn("10100", "10000");
     #else
     mySwitch.switchOn("10100", "10000");
-    logger("RFC On",Debug);
+    logger("RFC On\n",Debug);
     //ESP.restart();
     #endif
   }
@@ -113,23 +275,26 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 void reconnect() {
   // Loop until we're reconnected
-    Serial.print("Attempting MQTT connection...");
+    logger("Attempting MQTT connection...",Info);
     // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
+      logger("connected\n",Info);
       // Once connected, publish an announcement...
       client.publish("RFC/outTopic", versionStr);
       // ... and resubscribe
       client.subscribe("RFC/inTopic");
       client.subscribe("test/switch");
+      client.subscribe(charMqttTopic_433_Cmnd); // house/433/ardu1/cmnd
+      logger("MQTT sub >",Info);
+      logger(charMqttTopic_433_Cmnd,Info);
       MQTTConnection = true;
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
+      logger("failed, rc=",Warning);
+      logger(client.state(),Warning);
+      logger(" try again in 5 seconds\n",Warning);
       // Wait 5 seconds before retrying
       MQTTConnection = false;
     }
@@ -203,7 +368,7 @@ void setup() {
   TelnetServer.setNoDelay(true);
 
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  client.setCallback(callback2);
   mySwitch.enableTransmit(GPIO_0);
   mySwitch.setRepeatTransmit(TRANSMIT_RETRY);
   // OTA (only after connection is established)
@@ -295,12 +460,12 @@ void loop() {
          */
       if(cnt%2==0)
       {
-        logger("Toggle On",Debug);
+        logger("Toggle On\n",Debug);
         //mySwitch.switchOn("10101", "10000");
         //mySwitch.switchOn("10100", "10000");
         //digitalWrite(GPIO_D5, HIGH); 
       }else{
-        logger("Toggle Off",Debug);
+        logger("Toggle Off\n",Debug);
         //mySwitch.switchOff("10101", "10000");
         //mySwitch.switchOff("10100", "10000");
         //digitalWrite(GPIO_D5, LOW);
@@ -309,12 +474,22 @@ void loop() {
   }
 }
 
-void TelnetMsg(String text){
+void TelnetMsg(char* text){
   for(i = 0; i < MAX_TELNET_CLIENTS; i++)
   {
     if (TelnetClient[i] || TelnetClient[i].connected())
     {
-      TelnetClient[i].println(text);
+      TelnetClient[i].print(text);
+    }
+  }
+  delay(10);  // to avoid strange characters left in buffer
+}
+void TelnetMsg(char text){
+  for(i = 0; i < MAX_TELNET_CLIENTS; i++)
+  {
+    if (TelnetClient[i] || TelnetClient[i].connected())
+    {
+      TelnetClient[i].print(text);
     }
   }
   delay(10);  // to avoid strange characters left in buffer
@@ -394,12 +569,32 @@ void Telnet(){
     }
   }
 }
-
+/*
 void logger(String logInput,uint8_t level){
   //uint8_t logLevel = level;
   if(UsedLevel <= level){
-    Serial.println(logInput);
+    Serial.print(logInput);
+    TelnetMsg((char*)logInput);
+  }
+}*/
+void logger(char logInput,uint8_t level){
+  //uint8_t logLevel = level;
+  if(UsedLevel <= level){
+    Serial.print(logInput);
     TelnetMsg(logInput);
   }
-
+}
+void logger(char* logInput,uint8_t level){
+  //uint8_t logLevel = level;
+  if(UsedLevel <= level){
+    Serial.print(logInput);
+    TelnetMsg(logInput);
+  }
+}
+void logger(const char* logInput,uint8_t level){
+  //uint8_t logLevel = level;
+  if(UsedLevel <= level){
+    Serial.print(logInput);
+    TelnetMsg((char*)logInput);
+  }
 }
